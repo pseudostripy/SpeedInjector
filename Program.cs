311e5c19e1a5bb5a8fdc32e28e34f9d17298cac0
pseudostripy <pseudostripy@gmail.com>
2023-02-02 20:56:33 -0500

    Tidying up the main program to remove debug statements / printlines

--- Program.cs
+++ Program.cs
@@ -8,24 +8,15 @@ namespace Injector
 {
     public class Program
     {
+        private const int Pause_ms = 25;
+        private const int Timeout_s = 3;
         public static int Main(string[] args)
         {
-#if DEBUG
-            var dllpath = "C:\\Users\\adaml\\Documents\\Coding\\meta_speedhack\\DS2S META\\bin\\Debug\\net6.0-windows\\DS2S META 0.7.0.0\\Resources\\DLLs\\x86\\Speedhack.dll";
-#else
             if (args.Length != 1)
                 return -2;
 
             var dllpathraw = args[0];
             var dllpath = dllpathraw.Replace("\"", ""); // remove required wrapper quotes for IPC
-#endif
-
-
-            // checking inputs
-            //using StreamWriter file = new("WriteLines2.txt");
-            //file.WriteLine(dllpath);
-            //file.Close();
-
 
             var Hook = new DS2Hook(50, 50)
             {
@@ -33,8 +24,9 @@ namespace Injector
             };
             Hook.Start();
 
-            var pause = new TimeSpan(0, 0, 0, 0, 25); // ms between attempts
-            var timeout = new TimeSpan(0, 0, 0, 3); // seconds timeout period
+            
+            var pause = new TimeSpan(0, 0, 0, 0, Pause_ms); // ms between attempts
+            var timeout = new TimeSpan(0, 0, 0, Timeout_s); // seconds timeout period
             var injected = RetryUntilSuccessOrTimeout(() => Hook.IsInjected, timeout, pause);
 
             // Not sure its a good idea to return this way
